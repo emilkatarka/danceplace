51a6cb51997c8ade378caa4383c37c11b8fc69f8
emilkatarka <emilkatarka@gmail.com>
2023-01-24 22:19:30 +0100

    django debug toolbar and css for background and font

--- config/settings.py
+++ config/settings.py
@@ -141,11 +141,10 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-
-def show_toolbar(request):
-    return True
-
-
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': show_toolbar,
-}
+# def show_toolbar(request):
+#     return True
+#
+#
+# DEBUG_TOOLBAR_CONFIG = {
+#     'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+# }
