99112034ce3d99b8ac7143febe6c5ca95dfce158
emilkatarka <emilkatarka@gmail.com>
2023-01-28 00:34:25 +0100

    registration with custom form and view

--- users/views.py
+++ users/views.py
@@ -1,2 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from users import forms
+
+
+def registration_view(request):
+    if request.method == 'POST':
+        form = forms.RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.save()
+            return redirect('login')
+    else:
+        form = forms.RegistrationForm()
+
+    return render(request, 'users/registration.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
