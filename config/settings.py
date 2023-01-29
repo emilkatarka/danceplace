4e0755c7baf26796be5873c30a5d685e7c90a2ce
emilkatarka <emilkatarka@gmail.com>
2023-01-30 00:51:31 +0100

    environmental variables for django secret key conf

--- config/settings.py
+++ config/settings.py
@@ -12,15 +12,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-47b)a)&pu+pj0___(5+)9*!f=xx@9_o6y3quvo+jwfnukkk7bi'
-
+SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
+print('*' * 20)
+print(SECRET_KEY)
+print('*' * 20)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
